7359007ddf31e22b90d47be5834e723961b4fcdc
Rami1x <92189143+Rami1x@users.noreply.github.com>
2024-09-16 14:26:56 -0600

    added more stff but it broken i fix later

--- main.c
+++ main.c
@@ -7,6 +7,9 @@ void ipSum(char *a, char *b) {
 	int j = 0;
 	int seg = 0;
 	char ipArray[4][4] = {};
+	char ipArray2[4][4] = {};
+	char finalArray[4][4] = {};
+	char loserArray[4][4] = {};
 	
 	while (a[i] != '\0' && seg < 4) {
 		if (a[i] == '.') {
@@ -20,6 +23,41 @@ void ipSum(char *a, char *b) {
 	
 	for (int k = 0; k <= seg; k++) {
 		printf("%dst Segment:\t%s\n", k + 1, ipArray[k]);
+	}
+	
+	i = 0, j = 0, seg = 0;
+	
+	while (b[i] != '\0' && seg < 4) {
+		if (b[i] == '.') {
+			ipArray2[seg][j] = '\0';
+			seg++;
+			j = 0;
+		} else {
+			ipArray2[seg][j++] = b[i];
+		} i++;
+	} ipArray2[4][j] = '\0';
+	
+	for (int k = 0; k <= seg; k++) {
+		printf("%dst Segment:\t%s\n", k + 1, ipArray2[k]);
+	}
+	
+	i = 0, j = 0, seg = 0;
+	
+	for (int k = 0; k <= seg; k++) {
+		if (ipArray[seg][i] == ipArray2[seg][i] && ipArray[seg][i] == '\0') {
+			finalArray[seg][i] == '\0';
+			seg++;
+			i = 0;
+		} else if (ipArray[seg][i] != ipArray2[seg][i]){
+			loserArray[seg][i] = ipArray[seg][i];
+			i++;
+		} else {
+			finalArray[seg][i] = ipArray[seg][i];
+			i++;
+		}
+	}
+	for (int k = 0; k <= seg; k++) {
+		printf("final Array %s", finalArray[k]);
 	}
 }
 
